a2f916feba0fa5bf8b1da2cb4a8b33fab99b37f6
Kai Lawrence <mail@kailawrence.de>
2018-06-22 17:08:47 +0200

    Add label to index

--- docs/index/index.ext.js
+++ docs/index/index.ext.js
@@ -14,25 +14,37 @@ tests.push(
 
     new Test('label',
     `g2().view({cartesian:true})
-        .cir({x:75,y:75,r:50,ld:[6,3]})
-        .label({str:"n",loc:"n",off:10})
-        .label({str:"ne",loc:"ne",off:10})
-        .label({str:"e",loc:"e",off:10})
-        .label({str:"se",loc:"se",off:10})
-        .label({str:"s",loc:"s",off:10})
-        .label({str:"sw",loc:"sw",off:10})
-        .label({str:"w",loc:"w",off:10})
-        .label({str:"nw",loc:"nw",off:10})`),
+         .cir({x:75,y:75,r:50,ld:[6,3]})
+         .label({str:"c",loc:"c"})
+         .label({str:"n",loc:"n",off:10})
+         .label({str:"ne",loc:"ne",off:10})
+         .label({str:"e",loc:"e",off:10})
+         .label({str:"se",loc:"se",off:10})
+         .label({str:"s",loc:"s",off:10})
+         .label({str:"sw",loc:"sw",off:10})
+         .label({str:"w",loc:"w",off:10})
+         .label({str:"nw",loc:"nw",off:10})`),
 
     new Test('label2',
     `g2().view({cartesian:true})
-        .rec({x:50,y:30,b:100,h:75,ld:[6,3]})
-        .label({str:"n",loc:"n",off:10,border:true})
-        .label({str:"ne",loc:"ne",off:10,border:true})
-        .label({str:"e",loc:"e",off:10,border:true})
-        .label({str:"se",loc:"se",off:10,border:true})
-        .label({str:"s",loc:"s",off:10,border:true})
-        .label({str:"sw",loc:"sw",off:10,border:true})
-        .label({str:"w",loc:"w",off:10,border:true})
-        .label({str:"nw",loc:"nw",off:10,border:true})`),
+         .rec({x:50,y:30,b:100,h:75,ld:[6,3]})
+         .label({str:"c",loc:"c",border:true})
+         .label({str:"n",loc:"n",off:10,border:true})
+         .label({str:"ne",loc:"ne",off:10,border:true})
+         .label({str:"e",loc:"e",off:10,border:true})
+         .label({str:"se",loc:"se",off:10,border:true})
+         .label({str:"s",loc:"s",off:10,border:true})
+         .label({str:"sw",loc:"sw",off:10,border:true})
+         .label({str:"w",loc:"w",off:10,border:true})
+         .label({str:"nw",loc:"nw",off:10,border:true})`),
+
+    new Test('mark',
+    `g2().view({cartesian:true})
+         .cir({x:75,y:75,r:50,ld:[6,3]})
+         .mark({mrk:"dot",loc:["c","n","ne","e","se","s","sw","w","nw"]})`),
+
+    new Test('mark2',
+    `g2().view({cartesian:true})
+         .rec({x:50,y:30,b:100,h:75,ld:[6,3]})
+         .mark({mrk:"dot",loc:["c","n","ne","e","se","s","sw","w","nw"]})`)
 )
