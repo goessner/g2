fd45e7461a8a99d8660f332e49ee78c8c6781b4e
Stefan Goessner <stefan@goessner.net>
2020-05-07 12:12:53 +0200

    Update tests
    Some tests were removed, some were fixed.
    The code is also cleaner now.

--- docs/index/index.ext.js
+++ docs/index/index.ext.js
@@ -1,17 +1,9 @@
-new TestContainer("ext",
-     tests = [
+tests.push(
+    new Test ('g2.ext.js',
+    `g2().txt({str:"the following tests are for g2.ext.js",x:50,y:50})`),
 
-          new Test('spline',
-               `g2().spline({pts:[50,40,150,60,150,60],closed:false,lw:4,ls:'#080'})`),
-
-          new Test('spline-2',
-               `g2().spline({pts:[50,20,50,80,150,20],closed:false,lw:4,ls:'#080'})`),
-
-          new Test('spline-3',
-               `g2().spline({pts:[50,30,150,20,150,80,50,70],closed:true,lw:4,ls:'#080',fs:'orange'})`),
-
-          new Test('label',
-               `g2().view({cartesian:true})
+    new Test('label',
+    `g2().view({cartesian:true})
          .cir({x:75,y:75,r:50,ld:[6,3]})
          .label({str:"c",loc:"c"})
          .label({str:"n",loc:"n",off:10})
@@ -23,8 +15,8 @@ new TestContainer("ext",
          .label({str:"w",loc:"w",off:10})
          .label({str:"nw",loc:"nw",off:10})`),
 
-          new Test('label-2',
-               `g2().view({cartesian:true})
+    new Test('label2',
+    `g2().view({cartesian:true})
          .rec({x:50,y:30,b:100,h:75,ld:[6,3]})
          .label({str:"c",loc:"c",border:true})
          .label({str:"n",loc:"n",off:10,border:true})
@@ -36,8 +28,8 @@ new TestContainer("ext",
          .label({str:"w",loc:"w",off:10,border:true})
          .label({str:"nw",loc:"nw",off:10,border:true})`),
 
-          new Test('label-3',
-               `g2().view({cartesian:true})
+    new Test('label3',
+    `g2().view({cartesian:true})
         .pol({x:20,y:75}).label({str:'pol',loc:'s',off:25})
         .gnd({x:60,y:75}).label({str:'gnd',loc:'s',off:25})
         .nod({x:100,y:75}).label({str:'nod',loc:'s',off:25})
@@ -46,20 +38,19 @@ new TestContainer("ext",
         .nodflt({x:220,y:75}).label({str:'nodflt',loc:'s',off:25})
         .origin({x:260,y:75}).label({str:'origin',loc:'s',off:25})`),
 
-          new Test('mark',
-               `g2().view({cartesian:true})
+    new Test('mark',
+    `g2().view({cartesian:true})
          .cir({x:75,y:75,r:50,ld:[6,3]})
          .mark({mrk:"dot",loc:["c","n","ne","e","se","s","sw","w","nw"]})`),
 
-          new Test('mark-2',
-               `g2().view({cartesian:true})
+    new Test('mark2',
+    `g2().view({cartesian:true})
          .rec({x:50,y:30,b:100,h:75,ld:[6,3]})
          .mark({mrk:"dot",loc:["c","n","ne","e","se","s","sw","w","nw"]})`),
 
-          new Test('mark-3',
-               `g2().view({cartesian:true})
+    new Test('mark3',
+    `g2().view({cartesian:true})
          .ply({pts:[40,40,100,40,100,100,200,100,200,40]})
          .mark({mrk:"dot",loc:["beg",0.25,"mid",0.75,1]})
          .mark({mrk:"tick",loc:[0.1,0.3,0.5,0.7,0.9]})`),
-
-     ]);
+)
--- docs/index/index.mec.js
+++ docs/index/index.mec.js
@@ -1,48 +1,53 @@
-new TestContainer("mec",
-     tests = [
-          new Test('dim',
-               `g2().dim({x1:50,y1:75,x2:230,y2:75})
-     .dim({x1:50,y1:125,x2:230,y2:125,inside:false})`),
+tests.push(
+new Test ('g2.mec.js',
+    `g2().txt({str:"the following tests are for g2.mec.js",x:50,y:50})`),
 
-          new Test('dim-2',
-               `A = {x1: 30,y1:50,x2:200,y2:120}
+new Test('symbols',
+`g2().view({cartesian:true})
+     .pol({x:20,y:75})
+     .gnd({x:60,y:75})
+     .nod({x:100,y:75})
+     .dblnod({x:140,y:75})
+     .nodfix({x:180,y:75})
+     .nodflt({x:220,y:75})
+     .origin({x:260,y:75})`),
+
+new Test('vec',
+`g2().vec({x1:50,y1:20,x2:250,y2:120})`),
+
+new Test('dim',
+`g2().dim({x1:20,y1:75,x2:200,y2:75})
+     .dim({x1:20,y1:125,x2:200,y2:125,inside:false})`),
+
+new Test('dim2',
+`A = {x1: 30,y1:50,x2:200,y2:120}
 g2().lin({...A,ls:"red",lw:2})
      .dim({...A, off:-15,over:5})`),
 
-          new Test('adim',
-               `g2().adim({x:100,y:70,r:50,w:pi/3,dw:4*pi/3})
+new Test('adim',
+`g2().adim({x:100,y:70,r:50,w:pi/3,dw:4*pi/3})
      .adim({x:200,y:70,r:50,w:pi/3,dw:-4*pi/3,inside:false})`),
 
-          new Test('vec',
-               `g2().vec({x1:50,y1:20,x2:250,y2:120})`),
+new Test('slider',
+`g2().grid().slider({x:150,y:75,w:Math.PI/4,b:64,h:32})`),
 
-          new Test('avec',
-               `g2().avec({x: 220, y: 50, r: 30, w: 1, dw: -Math.PI})
-     .avec({x: 180, y: 80, w: Math.PI/2, dw: Math.PI, r: 50})
-     .lin({x1: 180, x2: 180, y1: 20, y2: 140})
-     .avec({x: 65, y: 65, r: 60, w: 1})
-     .avec({x: 80, y: 50, r: 30})`),
-
-          new Test('slider',
-               `g2().grid().slider({x:150,y:75,w:Math.PI/4,b:64,h:32})`),
-
-          new Test('spring',
-               `g2().spring({x1:50,y1:50,x2:200,y2:25})
+new Test('spring',
+`g2().spring({x1:50,y1:50,x2:200,y2:25})
      .spring({x1:50,y1:100,x2:200,y2:125,h:32})`),
 
-          new Test('damper',
-               `g2().damper({x1:120,y1:60,x2:240,y2:70})
+new Test('damper',
+`g2().damper({x1:120,y1:60,x2:240,y2:70})
      .damper({x1:50,y1:100,x2:200,y2:125,h:32})`),
 
-          new Test('link',
-               `let A = {x:50,y:25},B = {x:150,y:25},
+new Test('link',
+`let A = {x:50,y:25},B = {x:150,y:25},
      C = {x:50,y:75}, D = {x:100,y:75},
      E = {x:50,y:125};
 g2().view({cartesian:true})
     .link({pts:[A,B,E,A,D,C]})`),
 
-          new Test('link2',
-               `let A = {x:50,y:25},B = {x:150,y:25},
+new Test('link2',
+`let A = {x:50,y:25},B = {x:150,y:25},
     C = {x:50,y:75}, D = {x:100,y:75},
     E = {x:50,y:125};
 g2().view({cartesian:true})
@@ -53,57 +58,47 @@ g2().view({cartesian:true})
     .nod({...D,scl:1.5})
     .nodflt({...E,w:-Math.PI/2,scl:1.5})`),
 
-          new Test('beam',
-               `g2().view({cartesian:true})
+new Test('beam',
+`g2().view({cartesian:true})
      .beam({pts:[[200,125],[50,125],[50,50],[200,50]]})`),
 
-          new Test('beam2',
-               `g2().view({cartesian:true})
+new Test('beam2',
+`g2().view({cartesian:true})
      .beam2({pts:[[200,125],[50,125],[50,50],[200,50]]})`),
 
-          new Test('bar',
-               `g2().bar({x1:50,y1:20,x2:250,y2:120})`),
+new Test('bar',
+`g2().bar({x1:50,y1:20,x2:250,y2:120})`),
 
-          new Test('bar2',
-               `g2().bar2({x1:50,y1:120,x2:250,y2:120})`),
+new Test('bar2',
+`g2().bar2({x1:50,y1:120,x2:250,y2:120})`),
 
-          new Test('pulley',
-               `g2().view({cartesian:true})
+new Test('pulley',
+`g2().view({cartesian:true})
      .pulley({x:150,y:80,r:50})`),
 
-          new Test('pulley2',
-               `g2().view({cartesian:true})
+new Test('pulley2',
+`g2().view({cartesian:true})
      .pulley2({x:150,y:80,r:50})`),
 
-          new Test('rope',
-               `let A = {x:50,y:30}, B = {x:200,y:75};
+new Test('rope',
+`let A = {x:50,y:30}, B = {x:200,y:75};
 g2().view({cartesian:true})
     .pulley({...A,r:20}).pulley2({...B,r:40}).rope({p1:A,r1:20,p2:B,r2:40})`),
 
-          new Test('ground',
-               `g2().ground({pts:[25,25,25,75,75,75,75,25,125,25],pos:'left'})`),
+new Test('ground',
+`g2().ground({pts:[25,25,25,75,75,75,75,25,125,25],pos:'left'})`),
 
-          new Test('load',
-               `g2().view({cartesian:true})
+new Test('load',
+`g2().view({cartesian:true})
      .bar({x1:20,y1:50,x2:220,y2:50})
      .load({pts:[20,50,30,140,250,90,220,50],lw:1,w:-Math.PI/3,ls:"blue"})`),
 
-          new Test('load-2',
-               `g2().view({cartesian:true})
+new Test('load2',
+`g2().view({cartesian:true})
      .load({pts:[10,10,100,10,100,130,200,130,200,60,10,60],spacing:10})`),
 
-     new Test('pol',
-               `g2().view({cartesian:true})
-     .pol({x:20,y:75})
-     .gnd({x:60,y:75})
-     .nod({x:100,y:75})
-     .dblnod({x:140,y:75})
-     .nodfix({x:180,y:75})
-     .nodflt({x:220,y:75})
-     .origin({x:260,y:75})`),
-
-          new Test('mec',
-               `let A = {x:50,y:25},B = {x:150,y:25},
+new Test('mec',
+`let A = {x:50,y:25},B = {x:150,y:25},
     C = {x:50,y:75}, D = {x:100,y:75},
     E = {x:50,y:125};
 g2().view({cartesian:true})
@@ -121,4 +116,4 @@ g2().view({cartesian:true})
     .dim({x1:B.x+15,y1:E.y,x2:B.x+15,y2:D.y}).label({str:'b',loc:'w'})
     .dim({x1:B.x+15,y1:D.y,x2:B.x+15,y2:B.y}).label({str:'b',loc:'w'})`)
 
-     ]);
+)
